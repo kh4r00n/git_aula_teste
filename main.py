fdf46587f6794013b61a07f58c5490cba38b2cca
kh4r00n <caront3@icloud.com>
2022-03-14 10:01:55 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+display_messages = [
+    'Seja Feliz :)',
+    'Fique tranquilo, tudo vai dar certo!'
+]
+
+while True:
+    resposta = input('Deseja receber um conselho? S/N: ')
+    if resposta == 'S' or resposta == 's':
+        mensagem = random.choice(display_messages)
+        print(mensagem)
+        print()
